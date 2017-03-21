afbac7310a14deaec5fb15cb2fb6006ea3345e49
daoer <zhang1xuan@gmail.com>
2017-03-21 16:18:53 +0800

    testet
    tste

---
+++ SQLLP.sql
@@ -0,0 +1,12 @@
+SELECT  DISTINCT SRC from  info_lpcred
+WHERE MAFFTECH ='201606'
+AND banque=30056
+and cust_grp='CMB'
+--AND SRC ='REL'
+--AND maff_ouv ='200504'
+--AND maff_fin = '201704'
+;
+
+
+AUTRES : ORA-01830: date format picture ends before converting entire input string
+in rel
